f398b8e593402e61eb5fbe67ba8bbe1915be9e40
Toby S. Cubitt <toby-predictive@dr-qubit.org>
2012-04-25 14:05:45 +0200

    Updated copyright attribution and license (GPL2 -> GPL3).

--- trie.el
+++ trie.el
@@ -2,7 +2,7 @@
 ;;; trie.el --- trie package
 
 
-;; Copyright (C) 2008-2010, 2012 Toby Cubitt
+;; Copyright (C) 2008-2010, 2012  Free Software Foundation, Inc
 
 ;; Author: Toby Cubitt <toby-predictive@dr-qubit.org>
 ;; Version: 0.2.5
@@ -12,22 +12,20 @@
 ;; URL: http://www.dr-qubit.org/emacs.php
 
 
-;; This file is NOT part of Emacs.
+;; This file is part of Emacs.
 ;;
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 2
-;; of the License, or (at your option) any later version.
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 ;;
-;; This program is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-;; MA 02110-1301, USA.
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 
 ;;; Commentary:
